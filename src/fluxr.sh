3ed23abeb0b6840a731b375bbc92c402d5973b9b
niridium <niridium@tuta.io>
2026-05-20 21:14:23 +0200

    fix: ssh fixes and preparation for remote backups - prevent to run backup when host is treated as remote by mistake when inside ssh connection

--- src/fluxr.sh
+++ src/fluxr.sh
@@ -5,35 +5,25 @@ cd "$(dirname "$0")" || exit
 
 source fluxr.conf
 
-export ROOT BACKUP_DIR
-
-# Directories shown in the logs are relative to the host
-
-
+LOCAL_HOSTNAME=$(hostname)
 
 services_backup() {
     for services in "${SERVICES[@]}"; do
-        echo Backing up "$services -->"
-        ./services/"$services".sh
+        echo -e "\tBacking up $services -->"
+        ./"$services".sh
     done
     unset SERVICES
 }
 
 core_backup() {
-
-    if [[ $(hostname) == "$HOSTNAME" ]]; then
-        if [[ -z "$REMOTE_HOST" ]]; then
-
-            echo -e "\tBacking up $ROOT --> $BACKUP_DIR/$hostname/"
-            echo "---"
-            sudo rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR"/"$hostname"/
-        else
-
-            echo -e "\tBacking up $ROOT --> $REMOTE_HOST:$BACKUP_DIR/$hostname/"
-            echo "---"
-            sudo rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$REMOTE_HOST":"$BACKUP_DIR"/"$hostname"/
-            unset REMOTE_HOST
-        fi
+    if [[ "$LOCAL_HOSTNAME" == "$BACKUP_DIR_HOST" ]]; then
+        echo -e "\tBacking up $ROOT --> $BACKUP_DIR/$hostname/"
+        echo "---"
+        sudo rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR"/"$hostname"/
+    else
+        echo -e "\tBacking up $ROOT --> $BACKUP_DIR_HOST$BACKUP_DIR/$hostname/"
+        echo "---"
+        sudo rsync $RSYNC_OPTS --files-from=./"$hostname".include "$ROOT"/ "$BACKUP_DIR_HOST""$BACKUP_DIR"/"$hostname"/
     fi
 }
 
@@ -41,16 +31,28 @@ stage_1() {
     echo "+++ START STAGE 1"
     for hostname in "${HOSTNAMES[@]}"; do
         source "$hostname".conf
-        export hostname
 
         echo Host: "$hostname" "-->"
 
-        if $IS_REMOTE; then
+        if [[ "$LOCAL_HOSTNAME" != "$hostname" ]]; then
             echo -e "??? \tHost is remote"
-            # exit 2
+            if [[ -n "$SSH_TTY" ]]; then
+                echo -e "??? \tCan't backup ssh client"
+                continue
+            else
+                echo "---"
+
+                # To make this work I have to create arguments for the script,
+                # e.g., "./fluxr.sh $hostname", this will skip the for loop
+                # and only run for the current host when the script starts remotely
+                #
+                # ssh root@"$hostname" /storage/fluxr-test/fluxr.sh
+
+                continue
+            fi
         fi
 
-        if [[ -v "${SERVICES}" ]] then
+        if [[ -n "$SERVICES" ]]; then
             echo -e "\tThere are services to backup >>>"
             services_backup
         else
@@ -60,23 +62,5 @@ stage_1() {
     done
 }
 
-stage_2() {
-    echo "+++ START STAGE 2"
-
-    if [[ $(hostname) != "$BACKUP_DIR_HOST" && $(hostname) != "$PROPAGATION_1_HOST" ]]; then
-        echo -e "!!! \tCan't propagate within two remote directories"
-        exit 1
-    elif [[ $(hostname) == "$BACKUP_DIR_HOST" ]]; then
-        echo -e "??? \tBackup directory is local"
-        echo -e "??? \tPropagation directory is remote"
-        sudo rsync $RSYNC_OPTS $BACKUP_DIR/ $PROPAGATION_1_HOST:$PROPAGATION_1/
-    else
-        echo -e "??? \tBackup directory is remote"
-        echo -e "??? \tPropagation directory is local"
-        sudo rsync $RSYNC_OPTS $BACKUP_DIR_HOST:$BACKUP_DIR/ $PROPAGATION_1/
-    fi
-    echo "<><><> FLUX COMPLETED"
-    exit 0
-}
 stage_1
-stage_2
+./propagation.sh
